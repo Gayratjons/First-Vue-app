0f296279d7227ce18a3fed1539a2339a5f584a76
Umar_124 <umarbekkholmurodov@gmail.com>
2024-03-28 23:04:25 +0500

    firsrt

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <New />
+</template>
+
+<script>
+import New from './components/New.vue'
+
+export default {
+  name: 'App',
+  components: {
+    New
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
---
+++ src/components/New.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="div">
+        <button :class="variable" v-on:click="handleClick">
+            Click me
+        </button>
+    </div>
+    <h1 :id="id">Hello</h1>
+</template>
+
+<script>
+export default {
+    name: 'New',
+    data() {
+        return {
+            variable: 'Umar',
+            id: 'text'
+        }
+    },
+    methods: {
+        handleClick() {
+            const txt = document.getElementById("text");
+            txt.style.color = 'red'
+        }
+    },
+}
+</script>
+
+<style>
+.div {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+}
+
+/* .div {
+    width: 100px;
+    height: 100px; */
+/* } */
+</style>
